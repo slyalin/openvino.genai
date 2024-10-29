3ad376604348510377d55410d8bdb04ab332c200
Sergey Lyalin <sergey.lyalin@gmail.com>
2024-10-29 15:06:50 +0000

    Turn lora samples to benchmarks. Configure device and generation paramters (like max generated token or image size) in the code, it is not configurable from command line.

--- samples/cpp/text2image/lora.cpp
+++ samples/cpp/text2image/lora.cpp
@@ -3,31 +3,67 @@
 
 #include "openvino/genai/text2image/pipeline.hpp"
 
+#include <chrono>
+
 #include "imwrite.hpp"
 
 int32_t main(int32_t argc, char* argv[]) try {
-    OPENVINO_ASSERT(argc >= 3 && (argc - 3) % 2 == 0, "Usage: ", argv[0], " <MODEL_DIR> '<PROMPT>' [<LORA_SAFETENSORS> <ALPHA> ...]]");
+    OPENVINO_ASSERT(argc >= 4 && (argc - 4) % 2 == 0, "Usage: ", argv[0], " <MODEL_DIR> '<PROMPT>' [<LORA_SAFETENSORS> <ALPHA> ...] <MODE>");
 
     const std::string models_path = argv[1], prompt = argv[2];
     const std::string device = "CPU";  // GPU, NPU can be used as well
+    std::string mode = argv[argc-1];
+    std::cout << "MODE: " << mode << "\n";
 
-    ov::genai::AdapterConfig adapter_config;
+    std::map<std::string, ov::genai::AdapterConfig::Mode> modes = {
+        {"NO", ov::genai::AdapterConfig::MODE_AUTO},  // doesn't matter
+        {"EMPTY", ov::genai::AdapterConfig::MODE_DYNAMIC},
+        {"DYNAMIC", ov::genai::AdapterConfig::MODE_DYNAMIC},
+        {"STATIC", ov::genai::AdapterConfig::MODE_STATIC},
+        {"FUSE", ov::genai::AdapterConfig::MODE_FUSE},
+    };
+
+
+    ov::genai::AdapterConfig adapter_config(modes.at(mode));
     // Multiple LoRA adapters applied simultaneously are supported, parse them all and corresponding alphas from cmd parameters:
-    for(size_t i = 0; i < (argc - 3)/2; ++i) {
+    for(size_t i = 0; i < (argc - 4)/2; ++i) {
         ov::genai::Adapter adapter(argv[3 + 2*i]);
         float alpha = std::atof(argv[3 + 2*i + 1]);
         adapter_config.add(adapter, alpha);
     }
 
-    // LoRA adapters passed to the constructor will be activated by default in next generates
-    ov::genai::Text2ImagePipeline pipe(models_path, device, ov::genai::adapters(adapter_config));
+    using std::chrono::high_resolution_clock;
+    using std::chrono::milliseconds;
+    using std::chrono::duration_cast;
 
-    std::cout << "Generating image with LoRA adapters applied, resulting image will be in lora.bmp\n";
-    ov::Tensor image = pipe.generate(prompt,
+    // LoRA adapters passed to the constructor will be activated by default in next generates
+    auto t1 = high_resolution_clock::now();
+    ov::genai::Text2ImagePipeline pipe(models_path, device, mode == "NO" ? ov::AnyMap{} : ov::AnyMap{ov::genai::adapters(adapter_config)});
+    auto t2 = high_resolution_clock::now();
+    auto ms_int = duration_cast<milliseconds>(t2 - t1);
+    std::cout << "Initialization: " << ms_int.count() << "\n";
+
+    ov::AnyMap conf = {
         ov::genai::random_generator(std::make_shared<ov::genai::CppStdGenerator>(42)),
         ov::genai::width(512),
-        ov::genai::height(896),
-        ov::genai::num_inference_steps(20));
+        ov::genai::height(512),
+        ov::genai::num_inference_steps(20)
+    };
+
+    if(mode == "EMPTY") {
+        conf.insert(ov::genai::adapters());
+    }
+
+    for(size_t i = 0; i < 3; ++i) {
+        auto t1 = high_resolution_clock::now();
+        ov::Tensor image = pipe.generate(prompt, conf);
+        auto t2 = high_resolution_clock::now();
+        auto ms_int = duration_cast<milliseconds>(t2 - t1);
+        std::cout << "Iteration " << i + 1 << ": " << ms_int.count() << "\n";
+        imwrite("benchmark." + mode + "." + std::to_string(i+1) + ".bmp", image, true);
+    }
+
+    #if 0
     imwrite("lora.bmp", image, true);
 
     std::cout << "Generating image without LoRA adapters applied, resulting image will be in baseline.bmp\n";
@@ -38,6 +74,7 @@ int32_t main(int32_t argc, char* argv[]) try {
         ov::genai::height(896),
         ov::genai::num_inference_steps(20));
     imwrite("baseline.bmp", image, true);
+    #endif
 
     return EXIT_SUCCESS;
 } catch (const std::exception& error) {
